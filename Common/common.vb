5b7b214c1a9388bd6dd6f3d9d009912dd692b2e8
Rafael de Elvira <relviratellez@gmail.com>
2014-03-11 14:45:21 +0100

    forced committt

--- Common/common.vb
+++ Common/common.vb
@@ -8,35 +8,35 @@ Imports System.IO
 Namespace Common
 
     Module Common
-        Public Function UserLogin(ByVal User As String, ByVal Password As String)
+        Public Function UserLogin(ByVal user As String, ByVal password As String)
             Dim sqlManager As New MySQLManager
             Dim queryResult = sqlManager.ExecuteQuery("SELECT id, uid, password, email, full_name, profile_img, user_status FROM user where uid='" & User & "'", "user")
 
             Dim i As Integer = 0
-            Dim UserItem As New ClientData
+            Dim userItem As New ClientData
 
             If queryResult.Rows.Count > 0 Then
                 For Each oDataRow In queryResult.Rows
 
-                    If queryResult.Rows(i)("uid") = User And queryResult.Rows(i)("password") = getSHA1Hash(Password) Then
-                        UserItem.Id = queryResult.Rows(i)("id")
-                        UserItem.Passwd = queryResult.Rows(i)("password")
-                        UserItem.Name = queryResult.Rows(i)("uid")
-                        UserItem.Email = queryResult.Rows(i)("email")
-                        UserItem.FullName = queryResult.Rows(i)("full_name")
-                        UserItem.ImageSource = queryResult.Rows(i)("profile_img")
-                        UserItem.StateMessage = queryResult.Rows(i)("user_status")
-                        UserItem.isLoggedIn = True
+                    If queryResult.Rows(i)("uid") = user And queryResult.Rows(i)("password") = GetSha1Hash(password) Then
+                        userItem.Id = queryResult.Rows(i)("id")
+                        userItem.Passwd = queryResult.Rows(i)("password")
+                        userItem.Name = queryResult.Rows(i)("uid")
+                        userItem.Email = queryResult.Rows(i)("email")
+                        userItem.FullName = queryResult.Rows(i)("full_name")
+                        userItem.ImageSource = queryResult.Rows(i)("profile_img")
+                        userItem.StateMessage = queryResult.Rows(i)("user_status")
+                        userItem.isLoggedIn = True
                         Exit For
                     Else
-                        UserItem.isLoggedIn = False
+                        userItem.isLoggedIn = False
                     End If
                     i = i + 1
                 Next
             Else
-                UserItem.isLoggedIn = False
+                userItem.isLoggedIn = False
             End If
-            Return UserItem
+            Return userItem
         End Function
 
         Function IsEmail(ByVal email As String) As Boolean
@@ -45,7 +45,7 @@ Namespace Common
             Return emailExpression.IsMatch(email)
         End Function
 
-        Function getSHA1Hash(ByVal strToHash As String) As String
+        Function GetSha1Hash(ByVal strToHash As String) As String
 
             Dim sha1Obj As New SHA1CryptoServiceProvider
             Dim bytesToHash() As Byte = Encoding.ASCII.GetBytes(strToHash)
@@ -62,13 +62,13 @@ Namespace Common
 
         End Function
 
-        Function uploadUserLocalData(ByVal Uid As String)
+        Function UploadUserLocalData(ByVal uid As String)
             Dim SQLitePath = "sqlite/heydude.db"
             Try
 
                 'Upload image!
                 ServicePointManager.Expect100Continue = False
-                Dim uploadServer = DynamicServer & "sqliteUpload.php?uid=" & Uid
+                Dim uploadServer = DynamicServer & "sqliteUpload.php?uid=" & uid
 
                 Dim webp As New WebProxy("192.168.255.1", 3128)
                 webp.UseDefaultCredentials = True
@@ -84,10 +84,12 @@ Namespace Common
         End Function
 
         ' This is not very secure.... :S
-        Function downloadUserLocalFromServer(ByVal id As Integer, Optional ByVal Passwd As String = "")
+        Function DownloadUserLocalFromServer(ByVal id As Integer, Optional ByVal passwd As String = "")
             Dim SQLitePath = "sqlite/heydude.db"
             Try
                 ServicePointManager.Expect100Continue = False
+                System.Net.ServicePointManager.Expect100Continue = False
+                MessageBox.Show(ServicePointManager.Expect100Continue)
                 Dim downloadServer = DynamicServer & "downloadSqlite.php"
 
                 Dim webp As New WebProxy("192.168.255.1", 3128)
@@ -99,7 +101,7 @@ Namespace Common
                 'params
                 Dim reqparm As New Specialized.NameValueCollection
                 reqparm.Add("id", id)
-                reqparm.Add("passwd", Passwd)
+                reqparm.Add("passwd", passwd)
 
                 Dim responsebytes = webcl.UploadValues(downloadServer, "POST", reqparm)
 
